4e6283db0f55feb0471e3eee9c245b88310b08a7
Julie <juliediem@gmail.com>
2024-06-20 16:52:33 -0400

    Completed exercise

--- hirst-painting-start/main.py
+++ hirst-painting-start/main.py
@@ -1,10 +1,31 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
+import turtle
 import colorgram
+import random
+
+t = turtle.Turtle()
+turtle.colormode(255)
+t.penup()
+t.speed("fastest")
 
 rgb_colors = []
+rgb_tuples = []
 colors = colorgram.extract('image.jpg', 30)
+
 for color in colors:
     rgb_colors.append(color.rgb)
+    rgb_tuples.append((color.rgb.r, color.rgb.g, color.rgb.b))
 
-print(rgb_colors)
+print(rgb_tuples)
+
+# Starting position
+t.setpos(-250, -250)
+
+# Draw the dots
+for i in range(10):
+    for j in range(10):
+        t.dot(20, random.choice(rgb_tuples))
+        t.forward(50)
+    t.setx(-250)
+    t.sety(t.ycor() + 50)
+
+turtle.done()
