b2b655f2ae32e5540a26232f78da29e3291e8615
Julie <juliediem@gmail.com>
2024-06-21 13:23:11 -0400

    Start snake game

--- snake_game/main.py
+++ snake_game/main.py
@@ -8,19 +8,27 @@ screen.title("My Snake Game")
 
 
 def snake_body():
+    segments = []
     for i in range(0, 3):
         snake_part = Turtle(shape="square")
         snake_part.color("white")
+        snake_part.penup()
         x_cor = -20
         snake_part.setx(x_cor * i)
+        snake_part.sety(0)
+        print(snake_part.pos())
+        segments.append(snake_part)
+    return segments
 
-snake = snake_body()
-segments = [snake]
+
+new_snake = snake_body()
+print(new_snake)
 
 game_is_on = True
-
 while game_is_on:
-    for seg in segments:
+    for seg in new_snake:
         seg.forward(20)
+
+
 
 screen.exitonclick()
