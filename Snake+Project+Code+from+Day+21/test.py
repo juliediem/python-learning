138318976a51351fc2685c48d68353303eddfbb7
Julie <juliediem@gmail.com>
2024-06-24 15:08:21 -0400

    Snake game continued with file read function

---
+++ Snake+Project+Code+from+Day+21/test.py
@@ -0,0 +1,2 @@
+with open("new_file.text", mode="w") as file:
+    file.write("Hey this is my new file")
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -9,16 +9,23 @@ class Scoreboard(Turtle):
         self.color("white")
         self.setpos(0, 270)
         self.score = 0
+        self.high_score = 0
         self.scoreboard_text()
 
     def scoreboard_text(self):
-        self.write(f"Scoreboard: {self.score}", False, align="center", font=("Courier", 16, 'normal'))
+        self.clear()
+        self.write(f"Scoreboard: {self.score} Highscore: {self.high_score}", False, align="center", font=("Courier", 16, 'normal'))
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.scoreboard_text()
 
     def game_over(self):
         self.goto(0,0)
         self.write("GAME OVER", False, align="center", font=("Courier", 16, 'normal'))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.scoreboard_text()
